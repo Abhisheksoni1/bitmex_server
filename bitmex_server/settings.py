b96a68e2dd429bf0ca8d4045d0ef72ae574b24b6
abhisheksoni <abhisheksoni500@gmail.com>
2018-03-08 17:55:51 +0530

    heroku customization

--- bitmex_server/settings.py
+++ bitmex_server/settings.py
@@ -104,5 +104,5 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
